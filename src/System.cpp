0d60a687995d463731f9ef3b1cf088a03a50576f
yifei <yifei.yao@icloud.com>
2021-08-14 19:52:16 +0800

    Define System class

---
+++ src/System.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Yifei Yao on 8/14/21.
+//
+
+#include "System.h"
---
+++ src/System.h
@@ -0,0 +1,18 @@
+//
+// Created by Yifei Yao on 8/14/21.
+//
+
+#ifndef NBODY_SYSTEM_H
+#define NBODY_SYSTEM_H
+
+#include "Body.h"
+#include "vector"
+
+class System {
+public:
+private:
+    std::vector<Body *> objects;
+};
+
+
+#endif //NBODY_SYSTEM_H
