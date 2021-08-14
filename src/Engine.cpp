cdfa5d581acafb5e9a3c5d3a6010773c1a509d03
yifei <yifei.yao@icloud.com>
2021-08-14 20:23:35 +0800

    Add Engine class to handle System

---
+++ src/Engine.cpp
@@ -0,0 +1,7 @@
+//
+// Created by Yifei Yao on 8/14/21.
+//
+
+#include "Engine.h"
+
+Engine::Engine() : target(nullptr) {}
---
+++ src/Engine.h
@@ -0,0 +1,19 @@
+//
+// Created by Yifei Yao on 8/14/21.
+//
+
+#ifndef ENGINE_H
+#define ENGINE_H
+
+#include "System.h"
+
+class Engine {
+public:
+    Engine();
+
+private:
+    System *target;
+};
+
+
+#endif //ENGINE_H
