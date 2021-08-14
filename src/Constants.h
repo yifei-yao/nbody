05ee0189b1cb480e4a7812946a7484f41748e55b
yifei <yifei.yao@icloud.com>
2021-08-14 21:24:30 +0800

    Add Constants.h

---
+++ src/Constants.h
@@ -0,0 +1,12 @@
+//
+// Created by Yifei Yao on 8/14/21.
+//
+
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+namespace universal_constant {
+    const long double kGravitationalConstant = 6.67430e-11;
+}
+
+#endif //CONSTANTS_H
