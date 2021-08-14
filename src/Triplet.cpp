76b367fd214710422ea9ddc48e369e5956612d95
yifei <yifei.yao@icloud.com>
2021-08-14 19:32:06 +0800

    Define Triplet class

---
+++ src/Triplet.cpp
@@ -0,0 +1,8 @@
+//
+// Created by Yifei Yao on 8/14/21.
+//
+
+#include "Triplet.h"
+
+Triplet::Triplet(long double x, long double y, long double z) : x(x), y(y),
+                                                                z(z) {}
---
+++ src/Triplet.h
@@ -0,0 +1,20 @@
+//
+// Created by Yifei Yao on 8/14/21.
+//
+
+#ifndef TRIPLET_H
+#define TRIPLT_H
+
+
+class Triplet {
+public:
+    explicit Triplet(long double x = 0, long double y = 0, long double z = 0);
+
+private:
+    long double x;
+    long double y;
+    long double z;
+};
+
+
+#endif //TRIPLET_H
