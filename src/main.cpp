a8eb46a778396a5e7cba33f46eff29cf9125ce6f
yifei <yifei.yao@icloud.com>
2021-08-15 19:39:08 +0800

    Setup benchmarking tool

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@ int main() {
     Engine hyperdrive;
     hyperdrive.Load("demo.txt");
     auto start = std::chrono::steady_clock::now();
-    hyperdrive.Run(31622400, 10, "EulerImproved");
+    hyperdrive.Run(31622400, 1, "EulerImproved");
     auto end = std::chrono::steady_clock::now();
     hyperdrive.Save("test.txt");
     std::cout << "\n";
