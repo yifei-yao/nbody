a3d3f7909a3c5e65e75bdd42223a4b8026e8759a
yifei <yifei.yao@icloud.com>
2021-08-23 19:58:56 +0800

    Adjust main()

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,6 @@
 int main() {
     Engine hyperdrive;
     hyperdrive.Load("demo.txt");
-    hyperdrive.Run(31622400, 10, "RK4", "log.txt", true);
-//    hyperdrive.Translate("Barycenter");
-//    hyperdrive.Save("result.txt");
+    hyperdrive.Run(31622400, 10, "RK4", "log.txt", false);
+    hyperdrive.Save("result.txt");
 }
